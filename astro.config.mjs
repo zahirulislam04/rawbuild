a7e5d2079e325ba397074afed4f253e76697c6b7
zahirul islam <cse.zahirulislam@gmail.com>
2026-06-01 00:32:08 +0200

    remove base path for custom domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,7 +5,6 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://rawbuild.de',
-  base: '/rawbuild',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
