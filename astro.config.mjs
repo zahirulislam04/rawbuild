b45158192618a3b3e62633ee57a6a1d71b141046
zahirul islam <cse.zahirulislam@gmail.com>
2026-06-01 00:03:49 +0200

    fix asset paths

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
   site: 'https://rawbuild.de',
+  base: '/rawbuild',
   integrations: [sitemap()],
   vite: {
     plugins: [tailwindcss()],
