42e789c4f034728fc6a26cf795abc9774eb0669d
zahirul islam <cse.zahirulislam@gmail.com>
2026-06-03 23:57:20 +0200

    updated email address

--- src/data/contact.ts
+++ src/data/contact.ts
@@ -1,5 +1,5 @@
 export const contact = {
-  email: "cse.zahirulislam@gmail.com",
+  email: "info@rawbuild.de",
   githubUrl: "https://github.com/zahirulislam04",
   linkedinUrl: "https://www.linkedin.com/in/islam-mdzahirul",
 };
